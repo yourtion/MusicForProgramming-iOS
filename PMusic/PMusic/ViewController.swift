818e5de8f8e1f89f5f3db06a0e8db5e0ffea72f4
YourtionGuo <yourtion@gmail.com>
2015-12-09 14:32:15 +0800

    get musicList and show in table

--- PMusic/PMusic/ViewController.swift
+++ PMusic/PMusic/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    private var musics = Array<Music>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        MusicManager.sharedInstance.getList { (musicList) -> Void in
+            print(musicList)
+            self.musics = musicList
+            self.tableView.reloadData()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +31,16 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.musics.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("BasicCell")
+        cell?.textLabel?.text = self.musics[indexPath.row].title
+        return cell!
+    }
+    
+}
